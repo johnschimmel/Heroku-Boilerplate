5e21d3cce9a544de60d6f2ca2dbe6b74791d4364
John Schimmel <john.schimmel@gmail.com>
2013-02-15 09:16:23 -0500

    web.js cleanup

--- web.js
+++ web.js
@@ -26,18 +26,4 @@ http.createServer(function (req, res) {
 }).listen(port);
 
 // console info message
-console.log('Server running at http://127.0.0.1:8000/');
-
-
-// var express = require('express');
-
-// var app = express.createServer(express.logger());
-
-// app.get('/', function(request, response) {
-//   response.send('Hello Dynamic Web Class!');
-// });
-
-// var port = process.env.PORT || 3000;
-// app.listen(port, function() {
-//   console.log("Listening on " + port);
-// });
+console.log('Server running at http://127.0.0.1:' + port);
